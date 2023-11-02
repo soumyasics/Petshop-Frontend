c996cbf658cbb2aaba132647e2dc9992e084ddad
AnandRPSICS <anandrp.sics@gmail.com>
2023-11-02 12:44:40 +0530

    login page logo added

--- src/Pages/UserLogin/UserLoginPage.jsx
+++ src/Pages/UserLogin/UserLoginPage.jsx
@@ -1,7 +1,7 @@
 // 01-11-2023 Build by Anand R P
 import { useState } from "react";
 import loginDog from "../../Assets/Images/login-dog-img.png";
-
+import zookeper from "../../Assets/Images/zookeper-logo.png";
 import "./UserLoginPage.css";
 const LoginPage = () => {
   const [email, setEmail] = useState("");
@@ -17,6 +17,11 @@ const LoginPage = () => {
 
   return (
     <>
+    <div className="login-page-logo-container">
+      <img src={zookeper} alt="zookeper-logo" />  
+      <p>ZOOKEPER</p>
+
+    </div>
       <div className="login-page-container">
         <div className="login-page-left-section">
           <div className="login-form-container">
