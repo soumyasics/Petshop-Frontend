f5a6a6d9d78c9ae974febd16db4be95cf7c113f6
AnandRPSICS <anandrp.sics@gmail.com>
2023-11-18 02:01:22 +0530

    pet more info page design completed

--- src/Pages/PetMoreInfo/PetAdoptionCard.jsx
+++ src/Pages/PetMoreInfo/PetAdoptionCard.jsx
@@ -1,6 +1,14 @@
+import { Button, Modal } from "react-bootstrap";
+import { useState } from "react";
 import "./PetAdoptionCard.css";
 const PetAdoptionCard = ({ petData }) => {
-  console.log("pd", petData);
+  const [show, setShow] = useState(false);
+  const [buttonContent, setButtonContent] = useState("Adopt Me");
+  const [isAdopted, setIsAdopted] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   const { age, breed, description, gender, img, petname, price, shopid, type } =
     petData;
 
@@ -15,6 +23,12 @@ const PetAdoptionCard = ({ petData }) => {
   const dogPlaceholderImg = `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9L2cU1Xxu_XDcW-C0DueoXMgQ4W6qQO7xJ7K6gVw-IA&s`;
   const catPlaceholderImg = `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQednHyOH85OzO39f2ofViDcrVrF0U1JAWL0lN4KGPbyiO89GJgEy2oERXSIJ9M6cEDVuY&usqp=CAU`;
 
+  const confirmAdoption = () => {
+    handleClose();
+    alert(`${petname} Adoption Confirmed`);
+    setButtonContent("Adopted");
+    setIsAdopted(true);
+  };
   return (
     <div className="pet-adoption-card-container">
       <div className="pet-adoption-card-top">
@@ -60,9 +74,49 @@ const PetAdoptionCard = ({ petData }) => {
       <div className="pet-adoption-card-footer">
         <h2>About Me</h2>
         <p>{description}</p>
-        <p className="pet-adoption-card-footer-contact"> If you have any douts or need more information, please <span> Contact us </span></p>
-        <button className="adobpt-me-btn">Adopt Me </button>
+        <p className="pet-adoption-card-footer-contact">
+          {" "}
+          If you have any douts or need more information, please{" "}
+          <span> Contact us </span>
+        </p>
+        <button
+          onClick={handleShow}
+          disabled={isAdopted}
+          className={`adobpt-me-btn ${isAdopted ? "btn-disabled" : ""}`}
+        >
+          {buttonContent}
+        </button>
+ 
+ 
       </div>
+
+    
+      
+      <>
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Confirm {petname} Adoption</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <p>
+              Congratulations! You are about to make a wonderful decision by
+              adopting {petname}!
+            </p>
+            <p>
+              By clicking the "Pay ₹ {price}" button, you confirm your decision
+              to adopt {petname}.
+            </p>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Cancel
+            </Button>
+            <Button variant="primary" onClick={confirmAdoption}>
+              Pay ₹ {price}
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </>
     </div>
   );
 };
--- src/Pages/PetMoreInfo/PetMoreInfo.jsx
+++ src/Pages/PetMoreInfo/PetMoreInfo.jsx
@@ -34,7 +34,7 @@ const PetMoreInfo = () => {
   return (
     <div>
       {/* <UserNavbar /> */}
-      <NavbarUpdated/>
+      <NavbarUpdated />
       <div className="explore-header-section">
         <img src={catDogImg} alt="cat-dog" />
       </div>
@@ -44,7 +44,36 @@ const PetMoreInfo = () => {
         ) : (
           <div>Loading...</div>
         )}
-          
+
+        <div className="pet-adopt-photo-gallery">
+          <h2 className="">My Photo Gallery</h2>
+          <div className="pet-adopt-gallary-container">
+            <img
+              src="https://t4.ftcdn.net/jpg/01/99/00/79/360_F_199007925_NolyRdRrdYqUAGdVZV38P4WX8pYfBaRP.jpg"
+              alt="placeholder"
+            />
+            <img
+              src="https://t4.ftcdn.net/jpg/01/99/00/75/360_F_199007588_xYEG7WHU5776617DURpzPzwPD9y9Jik6.jpg"
+              alt="placeholder"
+            />
+            <img
+              src="https://as1.ftcdn.net/v2/jpg/01/99/00/78/1000_F_199007800_t4m2okwWCJ9U0chl75zxERWwovDatxLc.jpg"
+              alt="placeholder"
+            />
+            <img
+              src="https://i2-prod.irishmirror.ie/incoming/article26185096.ece/ALTERNATES/s615/0_I220209_151028_100570909oTextTRMRMMGLPICT000255932810o.jpg"
+              alt="placeholder"
+            />
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwaQjLLcXKSIuWnWq16hq11LsURY2jXVCl6_D9sK9g2PqEFRz2FIL7JtwDGyHmMGJXSsM&usqp=CAU"
+              alt="placeholder"
+            />
+            <img
+              src="https://media.graphassets.com/resize=height:360,width:1280/output=format:webp/ZBGEL9QYSyqE4PJwcCP9?width=1280"
+              alt="placeholder"
+            />
+          </div>
+        </div>
       </div>
       <NewsLetter />
       <Footer />
